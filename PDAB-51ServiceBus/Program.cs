cd28ce7872a30d4d3efa5ec1d7f1033ff8fdf5a3
Vidar Hagen <vidarh@hotmail.com>
2019-01-23 08:39:47 +0100

    Add project files.

---
+++ PDAB-51ServiceBus/Program.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PDAB_51ServiceBus
+{
+    class Program
+    {
+        //Must be updated to reflect your service
+        const string ServiceBusConnectionString = "<<Your Service Bus Connection String here>>";
+        const string QueueName = "salesmessages";
+
+        const int numberOfMessages = 10;
+        static IQueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            // Complete the message so that it is not received again.
+            // This can be done only if the queueClient is opened in ReceiveMode.PeekLock mode (which is default).
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            return Task.CompletedTask;
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the MessageHandler Options in terms of exception handling, number of concurrent messages to deliver etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Maximum number of Concurrent calls to the callback `ProcessMessagesAsync`, set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+
+                // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
+                // False value below indicates the Complete will be handled by the User Callback as seen in `ProcessMessagesAsync`.
+                AutoComplete = false
+            };
+
+            // Register the function that will process messages
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+
+        // Sends messages to the queue.
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            for (var i = 0; i < numberOfMessagesToSend; i++)
+            {
+                try
+                {
+                    // Create a new message to send to the queue
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue
+                    await queueClient.SendAsync(message);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+                }
+            }
+        }
+
+
+        static async Task MainAsync(string[] args)
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            // Register QueueClient's MessageHandler and receive messages in a loop
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            // Send Messages
+            await SendMessagesAsync(numberOfMessages);
+
+            Console.WriteLine("Press any key to exit after receiving all the messages.");
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+    }
+}
